356fae4f2e6a999be5362c520091f74f50749f8b
ludeasch <arnolchacho@yahoo.com.ar>
2017-05-11 14:39:23 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -21,7 +21,7 @@
 
 'use strict';                       
 
-const applicationServerPublicKey = 'tjtP21oYURXqTUoIkEMSsT3G-Pk_h2QOWldSZ2c33ZcSKOFIS28xLDCSxG2Ga45D';
+const applicationServerPublicKey = "BJh5Tr9tyUTlzOn13x5RDPQVO42qI7hWxt_oM0ALr1nJz9rJgpwg2leZJtidS-XJE7d1erqx6nkKoqsdkb7VwzQ";
 
 const pushButton = document.querySelector('.js-push-btn');
 
