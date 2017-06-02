b124f0ee93c1b62d45c5813f68e973527bd7bbde
Lucas Chiacchio <ludeas@bitbucket.org>
2017-06-02 15:59:31 -0300

    change

--- sw.js
+++ sw.js
@@ -48,8 +48,32 @@ self.addEventListener('fetch', function(event) {
     )
   );
 });
-
-
+document.querySelector('.cache-article').addEventListener('click', function(event) {
+  event.preventDefault();
+  debugger;
+  var id = this.dataset.articleId;
+  caches.open('mysite-article-' + id).then(function(cache) {
+    fetch('/get-article-urls?id=' + id).then(function(response) {
+      // /get-article-urls returns a JSON-encoded array of
+      // resource URLs that a given article depends on
+      return response.json();
+    }).then(function(urls) {
+      cache.addAll(urls);
+    });
+  });
+});
+document.querySelector('.cache-article').addEventListener('click', function(event) {
+  event.preventDefault();
+  event.waitUntil(
+    caches.open('.cache-article').then(function(cache) {
+      return cache.addAll(
+        [
+          'fallout.mp4',
+        ]
+      );
+    })
+  );
+});
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('example-cache').then(function(cache) {
